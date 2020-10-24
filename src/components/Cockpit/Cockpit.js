7e5fd571a6520ac4e44f5f00720a2910a0f0bf33
BibianaBalBar <balbuenabibi@gmail.com>
2020-10-24 23:22:50 +0100

    lifecycle hooks and useeffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+  useEffect (() => {
+    console.log('[Cockpit.js] useEffect');
+    setTimeout(() => {
+      alert("Saved data to cloud!");
+    }, 1000);
+    return () => {
+      console.log("[Cockpit.js] cleanup work in useEffect")
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log("[Cockpit.js] cleanup work in 2nd useEffect")
+    };
+  });
+
   const assignedClasses = [];
   let btnClass = '';
   
@@ -18,7 +35,7 @@ const cockpit = (props) => {
 
   return (
     <div className={classes.Cockpit}>
-      <h1>Hi, I'm a React App</h1>
+      <h1>{props.title}</h1>
       <p className={assignedClasses.join(" ")}>And it's really working!</p>
       <button 
         className={btnClass} 
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: "1", name: "Abo", age: 32 },
@@ -12,7 +17,26 @@ class App extends Component {
     ],
     otherState: "some other state",
     showPersons: false,
+    showCockpit: true,
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
 
   // To change name
   nameChangedHandler = (event, id) => {
@@ -48,6 +72,7 @@ class App extends Component {
 
 
   render() {
+    console.log("[App.js] render")
     let persons = null;    
 
     if (this.state.showPersons) {
@@ -64,10 +89,19 @@ class App extends Component {
 
     return (      
         <div className={classes.App}>   
-          <Cockpit 
+          <button
+          onClick={() => {
+            this.setState({ showCockpit: false });
+          }}>
+            Remove Cockpit
+          </button>
+          {this.state.showCockpit ? (
+            <Cockpit 
+            title={this.props.appTitle}
             showPersons={this.state.showPersons} 
             persons={this.state.persons}
             clicked={this.togglePersonsHandler} />       
+          ) : null}          
           {persons}
         </div>
       
